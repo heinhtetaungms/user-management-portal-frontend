f832e94ba41894efbb141dd49d276a4b7a4699ee
heinhtetaungms <hha.heinms@gmail.com>
2023-09-07 15:09:25 +0630

    add: tab name

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-user',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
+  private titleSubject = new BehaviorSubject<String>('Profile');
+  public titleAction$ = this.titleSubject.asObservable();
 
+  constructor() {
+  }
+
+  public changeTitle(title: string): void {
+    this.titleSubject.next(title);
+  }
 }
